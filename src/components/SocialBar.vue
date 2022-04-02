4c6cad8be4c08323e6478c293e6886804cf9eeee
entryword <entryword@gmail.com>
2022-04-02 16:49:38 +0200

    remove part don't like

--- src/components/SocialBar.vue
+++ src/components/SocialBar.vue
@@ -2,16 +2,6 @@
   <div class="social-wrap">
     <ul>
       <li>
-        <a :href="links.metadata.facebook" target="_blank">
-          <font-awesome-icon class="icon" :icon="['fab', 'facebook-square']" />
-        </a>
-      </li>
-      <li>
-        <a :href="links.metadata.instagram" target="_blank">
-          <font-awesome-icon class="icon" :icon="['fab', 'instagram-square']" />
-        </a>
-      </li>
-      <li>
         <a :href="links.metadata.linkedin" target="_blank">
           <font-awesome-icon class="icon" :icon="['fab', 'linkedin']" />
         </a>
